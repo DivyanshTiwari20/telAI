241ef99e6cf9fe7b31fff7bb7c504e9178238012
DivyanshTiwari20 <tmadhuijay1982@gmail.com>
2024-10-08 11:59:12 +0530

    fixed spelling error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Chat from './components/chat';
+import Chat from './components/Chat';
 
 function App() {
   return (
